9f8a32da28bce31121cdfe7fff5e998921ca9dde
Manuel Sommerhalder <manuel.sommerhalder@goldinteractive.ch>
2017-04-14 23:58:04 +0200

    Add outerHeight and outerWidth dom helper Add pushState/replaceState to change url and trigger popstate event to global event hub when url changes

--- src/utils/url.js
+++ src/utils/url.js
@@ -3,6 +3,8 @@
  * @module base/utils/url
  */
 
+import eventHub from '../eventHub'
+
 /**
  * Initial search parameters.
  * @type {Object}
@@ -34,6 +36,25 @@ export function hash(href) {
   return (href || window.location.hash).substring(1)
 }
 
+export function pushState(url, state = null, title = '') {
+  window.history.pushState(state, title, url)
+  eventHub.trigger('pushstate', state).trigger('statechange', state)
+}
+
+export function replaceState(url, state = null, title = '') {
+  window.history.replaceState(state, title, url)
+  eventHub.trigger('replacestate', state).trigger('statechange', state)
+}
+
+// map popstate event to event hub
+window.addEventListener('popstate', function(e) {
+  eventHub.trigger('popstate', e.state).trigger('statechange', e.state)
+})
+
 export default {
-  params, hash, getParams
+  params,
+  hash,
+  getParams,
+  pushState,
+  replaceState
 }
