7bb479c97738a84248dc58a398c6f3a18d26b2fa
Manuel Sommerhalder <manuel.sommerhalder@goldinteractive.ch>
2017-09-18 11:21:29 +0200

    Some array helper methods

--- src/utils/array.js
+++ src/utils/array.js
@@ -48,9 +48,79 @@ export function unique(array) {
     return out
 }
 
+/**
+ * Shuffle given array.
+ *
+ * @param   {Array} array - Target array.
+ * @returns {Array} Shuffled array.
+ */
+export function shuffle(array) {
+  return array.slice().sort(function() {
+    return Math.random() > 0.5 ? 1 : -1
+  })
+}
+
+/**
+ * Clone given array.
+ *
+ * @param   {Array} array - Target array.
+ * @returns {Array} Cloned array.
+ */
+export function clone(array) {
+  return array.slice(0)
+}
+
+/**
+ * Return largest number from given array.
+ *
+ * @param   {Number[]} array - Target array.
+ * @returns {Number} Largest number of given array.
+ */
+export function max(array) {
+  return Math.max.apply(Math, this)
+}
+
+/**
+ * Return smallest number from given array.
+ *
+ * @param   {Number[]} array - Target array.
+ * @returns {Number} Smallest number of given array.
+ */
+export function min(array) {
+  return Math.min.apply(Math, this)
+}
+
+/**
+ * Return sum from given array.
+ *
+ * @param   {Number[]} array - Target array.
+ * @returns {Number} Total sum of given array.
+ */
+export function sum(array) {
+  return array.reduce(function(a, b) {
+    return a + b
+  })
+}
+
+/**
+ * Return average from given array.
+ *
+ * @param   {Number[]} array
+ * @returns {Number} Average of given array.
+ */
+export function avg(array) {
+  var arraySum = sum(array)
+  return arraySum / array.length
+}
 
 export default {
+  shuffle,
   contains,
   random,
-  unique
+  unique,
+  clone,
+  max,
+  min,
+  sum,
+  avg
 }
