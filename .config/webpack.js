01a1937f368531895633a50a49fe641c6701c21c
Simon Stucki <simon_stucki@hotmail.com>
2019-02-18 08:59:41 +0100

    Use webpack v4

--- .config/webpack.js
+++ .config/webpack.js
@@ -1,58 +1,29 @@
-var webpack = require('webpack')
-var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin
-var path = require('path')
+const path = require('path')
 
 const root = path.join(__dirname, '..')
-const libraryName = process.env.LIBRARY_NAME
 
-var plugins = [
-  new webpack.LoaderOptionsPlugin({
-    options: {
-      context: root
-    }
-  })
-]
+const LIBRARY_NAME = 'base'
 
 module.exports = function(env = {}) {
   var entry = path.join(root, process.env.SOURCE_PATH, 'index.js')
   var outputPath = path.join(root, process.env.LIBRARY_PATH)
-  var outputFile = libraryName + '.js'
+  var outputFile = LIBRARY_NAME + '.js'
 
-  env.mode = env.mode || 'default'
+  env.mode = env.mode || 'development'
 
-  if (env.mode === 'minified') {
-    plugins.push(
-      new UglifyJsPlugin({
-        sourceMap: true,
-        compress: {
-          warnings: true,
-          unused: false
-        },
-        minimize: true
-      })
-    )
-
-    plugins.push(
-      new webpack.LoaderOptionsPlugin({
-        minimize: true
-      })
-    )
-
-    outputFile = libraryName + '.min.js'
-  }
-
-  if (env.mode == 'dev') {
-    entry = path.join(root, process.env.DEV_PATH, 'demo.js')
-    outputPath = path.join(root, process.env.DEV_PATH)
-    outputFile = 'demo.bundle.js'
+  if (env.mode == 'production') {
+    outputFile = LIBRARY_NAME + '.min.js'
   }
 
   var config = {
+    mode: env.mode,
     entry: entry,
-    devtool: 'source-map',
     output: {
       path: outputPath,
-      filename: outputFile
+      filename: outputFile,
+      library: LIBRARY_NAME,
+      libraryTarget: 'umd',
+      umdNamedDefine: true
     },
     module: {
       rules: [
@@ -66,14 +37,7 @@ module.exports = function(env = {}) {
     resolve: {
       modules: [path.join(root, 'src'), path.join(root, 'node_modules')],
       extensions: ['.js']
-    },
-    plugins: plugins
-  }
-
-  if (env.mode != 'dev') {
-    config.output.library = libraryName
-    config.output.libraryTarget = 'umd'
-    config.output.umdNamedDefine = true
+    }
   }
 
   return config
