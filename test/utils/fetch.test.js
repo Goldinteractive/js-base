108bcf6a09f728e917c798b63a4258bbabb0aaef
Simon Stucki <simon_stucki@hotmail.com>
2019-02-28 09:27:18 +0100

    Add url test suite

---
+++ test/utils/fetch.test.js
@@ -0,0 +1,21 @@
+import { url } from 'utils/fetch'
+
+describe('url', () => {
+  let mockFetchPromise
+  beforeEach(() => {
+    mockFetchPromise = jest.fn()
+    mockFetchPromise.mockReturnValue(Promise.resolve())
+    jest.spyOn(global, 'fetch').mockImplementation(mockFetchPromise)
+  })
+  test('appends query param', () => {
+    url('', { queryParams: { a: 1 } })
+    expect(mockFetchPromise).toHaveBeenCalledWith('?a=1', expect.any(Object))
+  })
+  test('checks existing query param', () => {
+    url('?b=2', { queryParams: { a: 1 } })
+    expect(mockFetchPromise).toHaveBeenCalledWith(
+      '?b=2&a=1',
+      expect.any(Object)
+    )
+  })
+})
