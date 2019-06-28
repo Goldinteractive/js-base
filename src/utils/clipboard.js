d8773c6e736f6400b47b688b29c70b899c9b47db
Simon Stucki <simon_stucki@hotmail.com>
2019-06-28 16:21:36 +0200

    Utils: Fix clipboard on iOS
    Fixes #6

--- src/utils/clipboard.js
+++ src/utils/clipboard.js
@@ -3,6 +3,21 @@
  * @module base/utils/clipboard
  */
 
+// https://stackoverflow.com/a/5379408
+function getSelection() {
+  var text = ''
+  if (window.getSelection) {
+    text = window.getSelection().toString()
+  } else if (document.selection && document.selection.type != 'Control') {
+    text = document.selection.createRange().text
+  }
+  return text
+}
+
+function checkSelection(str) {
+  return getSelection() === str
+}
+
 /**
  * Copies a string to the clipboard.
  * Notice, that this operation *must* be executed upon user
@@ -10,7 +25,7 @@
  * from: https://github.com/Chalarangelo/30-seconds-of-code#copytoclipboard-
  * @param {string} str - text to copy to clipboard
  */
-export function copyToClipboard (str) {
+export function copyToClipboard(str) {
   const el = document.createElement('textarea')
   el.value = str
   el.setAttribute('readonly', '')
@@ -22,6 +37,12 @@ export function copyToClipboard (str) {
       ? document.getSelection().getRangeAt(0)
       : false
   el.select()
+  if (!checkSelection(str)) {
+    // if selection failed, try it another way
+    // at the time of writing, iOS devices fail to select text using el.select()
+    // this fallback version should only then be used.
+    el.setSelectionRange(0, 999999)
+  }
   document.execCommand('copy')
   document.body.removeChild(el)
   if (selected) {
