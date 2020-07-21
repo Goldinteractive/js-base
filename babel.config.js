2b1f8fdf6a62eef0f923a5cb24f77419ff8febee
Marc <marc.berli@windowslive.com>
2020-07-21 13:57:45 +0200

    Enable nullish coalescing and optional chaining

--- babel.config.js
+++ babel.config.js
@@ -7,7 +7,9 @@ module.exports = function(api) {
   const presets = [['@babel/preset-env', envOptions]]
   const plugins = [
     ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
-    '@babel/plugin-proposal-class-properties'
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-nullish-coalescing-operator'
   ]
 
   return {
